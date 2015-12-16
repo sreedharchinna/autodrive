659d5c117a4787b3c9883a2ecefd805e3748ba20
sreedharchinna <sreedharchinna@gmail.com>
2015-12-16 21:34:48 +0530

    changes

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ var _ = require("underscore");
 var autodrive = {
     between: function(low, high){
         return _.random(low, high);
-    }
+    };
+    execute: function(jsonfile){
+    
+    };
     
 };
 
--- test/test.js
+++ test/test.js
@@ -1,3 +1,3 @@
 var autodrive = require('autodrive');
-autodrive.execute("mastertrees\\google_search.json");
-
+//autodrive.execute("mastertrees\\google_search.json");
+autodrive.between(1,10);
