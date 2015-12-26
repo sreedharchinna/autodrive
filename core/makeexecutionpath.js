44ceb3c078dce9630be35391aba0aa70bcf0b8de
sreedharchinna <sreedharchinna@gmail.com>
2015-12-26 19:18:07 +0530

    ne changes

--- core/makeexecutionpath.js
+++ core/makeexecutionpath.js
@@ -1,13 +1,29 @@
 var jr = require('.//readjson.js');
+var util = require('util');
 var path = {
     makePath: function(jsonfile, step){
         
-        console.log("Inside 'makeexecutionpath' function");
-        console.log("JSON file :", jsonfile);
-        console.log("Step id :", step);
+        util.log("Inside 'makeexecutionpath' function");
+        util.log("JSON file :", jsonfile);
+        util.log("Step id :", step);
         
         var jsobject = jr.readJson(jsonfile);
         console.log(jsobject);
+        for(var pathKey in jsobject) {
+                util.log("key is "+pathKey);
+                util.log("root id is: "+jsobject[pathKey].id);
+                if(jsobject[pathKey].id != 1){
+                    console.error("Root id is not one, check the json file.");
+                }
+                //console.log(jsobject[pathKey].children.length);
+                if("children" in jsobject[pathKey]){
+                    util.log("children exists");
+                    util.log("Number of children"+jsobject[pathKey].children.length);
+                }
+//              if(jsobject[pathKey].children.length)
+                //console.log("value: "+jsobject[pathKey].children[0].id);
+                //console.log("value: "+jsobject[pathKey].children[1].id);                
+            }
     }
 };
 module.exports = path;
--- core/readjson.js
+++ core/readjson.js
@@ -15,19 +15,6 @@ var core = {
             console.log("json data ",jsonData);          
             jsobject = parser.parse(jsonData);
             console.log(jsobject);
-            
-            for(var pathKey in jsobject) {
-                console.log("key is "+pathKey);
-                console.log("value: "+jsobject[pathKey].id);
-                //console.log(jsobject[pathKey].children.length);
-                if("children" in jsobject[pathKey]){
-                    console.log("children exists");
-                    console.log("Number of children"+jsobject[pathKey].children.length);
-                }
-//              if(jsobject[pathKey].children.length)
-                //console.log("value: "+jsobject[pathKey].children[0].id);
-                //console.log("value: "+jsobject[pathKey].children[1].id);                
-            }            
         }   
         return jsobject;
     }
