2ac8412646e8c4140d7414e0b1a87e45fddf7e4b
sreedharchinna <sreedharchinna@gmail.com>
2016-01-05 18:11:20 +0530

    change to execution

--- core/makeexecutionpath.js
+++ core/makeexecutionpath.js
@@ -1,14 +1,56 @@
 var jr = require('.//readjson');
 var util = require('util');
 var path = {
-    makePath: function(jsonfile, step){
+    makePath: function (jsonfile, step) {
         
         util.log("Inside 'makeexecutionpath' function");
         util.log("JSON file :", jsonfile);
         util.log("Step id :", step);
         
-        var jsobject = jr.readJson(jsonfile);
+        var jsobject = jr.readJson(jsonfile), keyFound = false, containsID = false, containsNext = false, depth = 0;
+        
         console.log(jsobject);
+        //console.log(Object.keys(jsobject));
+        parseObj = jsobject;        
+        do {
+            console.log("-----------------------------------\n\n");
+            console.log("Current Object :" + parseObj);
+            tmpArr = Object.keys(parseObj);
+            for (var index in tmpArr){
+                if(tmpArr[index] === "id"){
+                    containsID = true;
+                    console.log("Contains ID ");
+                }                
+            }
+            if(containsID){
+                for(var index in tmpArr){
+                    if(tmpArr[index] === "next"){
+                        containsNext = true;
+                        console.log("Contains Next in ID:"+parseObj.id);
+                    }                
+                }
+            }
+            if(!containsID){
+                console.error("Input JSON format is wrong: does not contain ID in a step.");
+                return false;
+            }
+            console.log("Current ID :"+parseObj.id);
+            if(parseObj.id == step){
+                console.log("Found :"+parseObj.id);
+                return true;
+            }
+            if(containsNext){
+                parseObj = parseObj.next; 
+                console.log("Next Step :"+parseObj);
+            } 
+        }while(containsNext);
+        
+//        for(var pathKey in jsobject) {
+//            if(pathKey === "id"){
+//                
+//                console.log("root id is: "+jsobject[pathKey]);
+//            }
+//        }
 //        for(var pathKey in jsobject) {
 //                util.log("key is "+pathKey);
 //                util.log("root id is: "+jsobject[pathKey].id);
